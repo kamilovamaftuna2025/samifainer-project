835ee27bea1ac1fad18855aaf0d05213b3e5988d
Maftuna Komilova <kamilovamaftuna2025@gmail.com>
2025-11-20 11:40:43 +0000

    bot ximoyalandi

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const TelegramBot = require("node-telegram-bot-api");
+const { config } = require("dotenv")
+config()
 
-const TOKEN = "8463114097:AAF-xiHtOyG_rErPDxz22jDA4X5dgkH1ZSM";
+const TOKEN = process.env.BOT_TOKEN;
 const bot = new TelegramBot(TOKEN, { polling: true });
 
 bot.onText(/\/start/, (msg) => {
@@ -44,26 +46,114 @@ bot.on('message', (msg) => {
 5️⃣ Grafik dizayn
 
 👇 Quyidagi kurslardan birini tanlang va batafsil ma’lumot oling:`, {
-      reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: "🇺🇸 Ingliz tili", callback_data: "kurs_ingliz" },
-                ],
-                [
-                    { text: "🇷🇺 Rus tili", callback_data: "kurs_rus" }
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "🇺🇸 Ingliz tili", callback_data: "kurs_ingliz" },
+                    ],
+                    [
+                        { text: "🇷🇺 Rus tili", callback_data: "kurs_rus" }
 
-                ]
-                [
-                    { text: "📗 Matematika", callback_data: "kurs_matematika" }
-                ],
-                [
-                    { text: " 💻 Dasturlash", callback_data: "kurs-dasturlash" }
+                    ],
+                    [
+                        { text: "📗 Matematika", callback_data: "kurs_matematika" }
+                    ],
+                    [
+                        { text: " 💻 Dasturlash", callback_data: "kurs-dasturlash" }
 
+                    ]
                 ]
-            ]
-        }
-});
+            }
+        });
+
+
     }
 });
 
-console.log("Bot ishga tushdi ✅");
+
+
+
+bot.on("callback_query", (query) => {
+    const chatId = query.message.chat.id
+    const data = query.data
+    if (data == "kurs_ingliz") {
+        bot.sendMessage(chatId, `🇬🇧 Ingliz tili kursi
+
+🕒 Davomiyligi: 3 oy
+🎯 Darajalar: Beginner – Intermediate
+📘 O‘quv dasturi: Speaking, Listening, Grammar
+📌 Har haftasi 3 ta dars
+💰 Narx: 250 000 so’m / oy`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "✍️ Ro‘yxatdan o‘tish", callback_data: "register_english" }
+                    ],
+                    [
+                        { text: "⬅️ Orqaga", callback_data: "back_to_courses" }
+                    ]
+                ]
+            }
+        })
+    }
+    else if (data == "kurs_rus") {
+        bot.sendMessage(chatId, `🇷🇺 Rus tili kursi
+🕒 Davomiyligi: 3 oy
+🎯 0 dan boshlovchilar uchun
+📘 So‘z boyligi, grammatikasi, og‘zaki nutq
+📌 Haftada 3 ta dars
+💰 Narx: 230 000 so’m / oy`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "✍️ Ro‘yxatdan o‘tish", callback_data: "register_english" }
+                    ],
+                    [
+                        { text: "⬅️ Orqaga", callback_data: "back_to_courses" }
+                    ]
+                ]
+            }
+        })
+    }
+    else if (data == "kurs_matematika") {
+        bot.sendMessage(chatId, `📗 Matematika kursi
+🕒 Davomiyligi: 4 oy
+🎯 Yo‘nalish: Maktab + Olimpiada
+📘 Algebra, Geometriya, Testlar
+📌 Haftada 3–4 dars
+💰 Narx: 300 000 so’m / oy`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "✍️ Ro‘yxatdan o‘tish", callback_data: "register_english" }
+                    ],
+                    [
+                        { text: "⬅️ Orqaga", callback_data: "back_to_courses" }
+                    ]
+                ]
+            }
+        })
+    }
+    else if (data == "kurs_dasturlash") {
+        bot.sendMessage(chatId, `💻 Dasturlash (Frontend)
+🕒 Davomiyligi: 5 oy
+📘 HTML, CSS, JavaScript, React
+🎯 Portfolio bilan bitirish
+📌 Haftada 3 ta dars
+💰 Narx: 450 000 so’m / oy`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "✍️ Ro‘yxatdan o‘tish", callback_data: "register" }
+                    ],
+                    [
+                        { text: "⬅️ Orqaga", callback_data: "back_to_courses" }
+                    ]
+                ]
+            }
+        })
+    }
+   
+})
+
+    console.log("Bot ishga tushdi ✅");
